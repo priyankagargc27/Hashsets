663c7ba2015d7dac66c6844aebc1f456f6862187
priyankagargc27 <40770470+priyankagargc27@users.noreply.github.com>
2018-10-04 11:05:03 -0500

    Hashsets practice in Csharp

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace sets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+HashSet<string> showroom = new HashSet<string>();
+showroom.Add("Lexus-rx350");
+showroom.Add("BMW-530");
+showroom.Add("Porche");
+showroom.Add("Highlander");
+showroom.Add("Highlander");
+Console.WriteLine("There are {0} cars in the showroom",showroom.Count);
+foreach (string vehicle in showroom)
+{
+    Console.WriteLine($"{vehicle}");
+}
+HashSet<string> usedLot = new HashSet<string>();
+ usedLot.Add("Tesla");
+ usedLot.Add("Volvo");
+  Console.WriteLine("usedLot unionwith showrrom");
+        showroom.UnionWith(usedLot);
+      showroom.Remove("Highlander"); 
+        foreach (string vehicle in showroom)
+{
+    Console.WriteLine($"{vehicle}");
+}
+
+
+
+
+        }
+    }
+}
